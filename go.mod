f4eb6d3061d417955150e0271f0214092680a2ad
Ayush Arora <ayusharora.282@gmail.com>
2024-12-25 15:32:20 +0530

    functionality done

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/cloudflare/circl v1.3.7 // indirect
 	github.com/cyphar/filepath-securejoin v0.2.4 // indirect
 	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/fatih/color v1.18.0 // indirect
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.5.0 // indirect
 	github.com/go-git/go-git/v5 v5.12.0 // indirect
@@ -16,6 +17,8 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pjbgf/sha1cd v0.3.0 // indirect
 	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
 	github.com/skeema/knownhosts v1.2.2 // indirect
@@ -25,7 +28,7 @@ require (
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/mod v0.12.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/tools v0.13.0 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
--- src-cmd/checkmyrepo/checkmyrepo.go
+++ src-cmd/checkmyrepo/checkmyrepo.go
@@ -1,7 +1,13 @@
 package checkmyrepoCmd
 
 import (
+	"fmt"
+	"log"
+	"sort"
+
+	"github.com/fatih/color"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/spf13/cobra"
 )
 
@@ -17,6 +23,73 @@ func cmdRun(cmd *cobra.Command, args []string) {
 	if err != nil {
 		cmd.PrintErrln("Error:this is not a git repo")
 	}
+	ref, err := repo.Head()
+	if err != nil {
+		log.Fatalf("Failed to get HEAD reference: %v", err)
+	}
+
+	commitIter, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+	if err != nil {
+		log.Fatalf("Failed to get commit history: %v", err)
+	}
+	authorChanges := make(map[string]int)
+	err = commitIter.ForEach(func(c *object.Commit) error {
+		fmt.Printf("Commit: %s\nAuthor: %s <%s>\nDate: %s\nMessage: %s\n\n",
+			c.Hash.String(),
+			c.Author.Name,
+			c.Author.Email,
+			c.Author.When,
+			c.Message,
+		)
+
+		stats, err := c.Stats()
+		if err != nil {
+			log.Fatalf("Failed to get Stats: %v", err)
+		}
+		totalChanges := 0
+		for _, stat := range stats {
+			totalChanges += stat.Addition + stat.Deletion
+		}
+		author := fmt.Sprintf("%s <%s>", c.Author.Name, c.Author.Email)
+		authorChanges[author] += totalChanges
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("Error while iterating commits: %v", err)
+	}
+
+	var totalChanges int
+
+	for _, change := range authorChanges {
+		totalChanges += change
+	}
+
+	type AuthorStat struct {
+		Author string
+		Equity float64
+	}
+	var stats []AuthorStat
+	for author, changes := range authorChanges {
+		equity := (float64(changes) / float64(totalChanges)) * 100
+		stats = append(stats, AuthorStat{Author: author, Equity: equity})
+	}
+	sort.Slice(stats, func(i, j int) bool {
+		return stats[i].Equity > stats[j].Equity
+	})
+	fmt.Println("Contribooters of this Repo:")
+	for _, stat := range stats {
+		redValue := uint8((1 - (stat.Equity / 100)) * 255)
+		greenValue := uint8((stat.Equity / 100) * 255)
+		// equityColor := color.New().Add(color.Attribute(38)).Add(color.Attribute(48)).
+		// 	Add(color.Attribute(5)).
+		// 	Add(color.Attribute(16 + int(redValue/36)*36 + int(greenValue/36)*6)).
+		// 	Sprint(fmt.Sprintf("%.2f%%", stat.Equity))
+		equityColor := color.RGB(int(redValue), int(greenValue), 0).Sprintf(fmt.Sprintf("%.2f%%%%", stat.Equity))
+		authorColor := color.New(color.FgMagenta).Sprint(stat.Author)
+
+		fmt.Printf("    Author: %s owns Code Equity: %s\n", authorColor, equityColor)
+	}
+
 }
 
 func Init() *cobra.Command {
